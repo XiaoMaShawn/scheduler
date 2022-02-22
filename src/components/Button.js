ff74f8315f6f416309b975e701116bb1e10ab95e
XiaoMaShawn <ma2014xiao@hotmail.com>
2022-02-22 16:14:39 -0500

    button/daylistitem components done

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,14 @@
-import React from "react";
-
 import "components/Button.scss";
+import React from "react";
+import classNames from "classnames";
+
 
 export default function Button(props) {
-   return <></>;
+
+   // use classNames function import from the dependency to generate the className string
+   let buttonClass = classNames('button', { 'button--confirm': props.confirm, 'button--danger': props.danger })
+
+   return (
+      <button className={buttonClass} onClick={props.onClick} disabled={props.disabled}>{props.children}</button>
+   );
 }
---
+++ src/components/DayListItem.js
@@ -0,0 +1,36 @@
+import "components/DayListItem.scss"
+import React from "react";
+import classNames from "classnames";
+
+const DayListItem = function(props) {
+
+  //event handler for set day
+  const setDay = props.setDay;
+  // const onClick = setDay(props.name);//this line, onClick is not a function, its just a reference, but onClick handler, you want the actually funtion there. Thats why the line below works.
+  const onClick = () => { setDay(props.name) };
+
+  //set the className for <li>
+  let liClass = classNames('day-list__item', { 'day-list__item--selected': props.selected, 'day-list__item--full': !props.spots })
+
+  //make a function to format the 'spots remaining' result
+  const formatSpots = (spots) => {
+    if (spots === 0) {
+      return 'no spots';
+    }
+    if (spots === 1) {
+      return '1 spot';
+    }
+    return `${spots} spots`;
+  }
+
+  return (
+    <li onClick={onClick} className={liClass}>
+
+      <h2 className="text--regular">{props.name}</h2>
+      <h3 className="text--light">{formatSpots(props.spots)} remaining</h3>
+
+    </li>
+  );
+}
+
+export default DayListItem;
